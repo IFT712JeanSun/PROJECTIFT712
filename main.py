2198431bbc100d209f1863bf4796ce4c4dd12bca
Jean-Paul <jeanplfaye@yahoo.fr>
2019-11-28 19:39:52 -0500

    Some function added to main

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 from plotting import *
 from submission import *
+from training import train_testPrecision_Predict
+
 
 def main():
-    #algo = train_testPrecision_Predict()
-    #print(' ***********      SANS TRANSFORMATION      **************')
-    #algo.train(transform=False)
-    #print(algo.testPrecision(transform=False))
-    #print('\n\n ***********   AVEC TRANSFORMATION      **************')
-    #algo.train(transform=True)
-    #print(algo.testPrecision(transform=True))
+    algo = train_testPrecision_Predict()
+    print(' ***********      SANS TRANSFORMATION      **************')
+    algo.train(transform=False)
+    print(algo.testPrecision(transform=False))
+    print('\n\n ***********   AVEC TRANSFORMATION      **************')
+    algo.train(transform=True)
+    print(algo.testPrecision(transform=True))
 
-    #print(algo.predict(model='LR'))
-    #print(algo.predict(model='SVM'))
-    #print(algo.predict(model='LDA'))
-    #print(algo.predict(model='DTC'))
-    #print(algo.predict(model='KNN'))
-    #print(algo.predict(model='NN'))
+    print(algo.predict(model='LR'))
+    print(algo.predict(model='SVM'))
+    print(algo.predict(model='LDA'))
+    print(algo.predict(model='DTC'))
+    print(algo.predict(model='KNN'))
+    print(algo.predict(model='NN'))
 
-
-    #compare, results, names = algo.checkAlgorithm()
-    #algo.boxplot()
-    #subm = algo.submission()
     toplot = Plotting()
     toplot.barPlot()
     toplot.scatterPlot()
