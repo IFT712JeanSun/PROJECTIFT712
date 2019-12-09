19bfa31bcea6ae83230cf6178f7a53a45af2d7f5
Jean-Paul <jeanplfaye@yahoo.fr>
2019-12-09 17:01:37 -0500

    More functions called to print of plot

--- main-run.py
+++ main-run.py
@@ -4,6 +4,15 @@ from training import train_testPrecision_Predict
 
 
 def main():
+    data = Data()
+    print("\n**********  Printing some data for quick observation ************\n")
+    data.printSomeData()
+    print("\n**********  Printing the shape of the data ************\n")
+    data.getShape()
+
+    print("\n********** Printing a quick description of the data *******\n")
+    print(data.dataDescription())
+
     algo = train_testPrecision_Predict()
     print(' ***********      SANS TRANSFORMATION      **************')
     algo.train(transform=False)
@@ -12,12 +21,12 @@ def main():
     algo.train(transform=True)
     print(algo.testPrecision(transform=True))
 
-    print(algo.predict(model='LR'))
-    print(algo.predict(model='SVM'))
-    print(algo.predict(model='LDA'))
-    print(algo.predict(model='DTC'))
-    print(algo.predict(model='KNN'))
-    print(algo.predict(model='NN'))
+    #print(algo.predict(model='LR'))
+    #print(algo.predict(model='SVM'))
+    #print(algo.predict(model='LDA'))
+    #print(algo.predict(model='DTC'))
+    #print(algo.predict(model='KNN'))
+    #print(algo.predict(model='NN'))
 
     # Also we can add ensemble here
 
@@ -25,6 +34,7 @@ def main():
     toplot.barPlot()
     toplot.scatterPlot()
 
+    print('\n\n ***********  Printing the submission  **************')
     sub = Submission()
     print(sub.submission())
 
